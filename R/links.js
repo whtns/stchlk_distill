fc1f54bb260de57b6abd353776cc6a099c5dd226
whtns <kevin.stachelek@gmail.com>
2022-10-25 12:58:16 -0700

    kstachelek

--- R/links.js
+++ R/links.js
@@ -14,7 +14,7 @@ window.onload = function(){
 $(document).ready(function(){
 	
    // external links to new window
-    $('a[href^="http://"]').not('a[href*="https://robcavanaugh.com"]').attr('target','_blank');
+    $('a[href^="http://"]').not('a[href*="http://stchlk.rbind.io"]').attr('target','_blank');
     
     // force PDF Files to open in new window
     $('a[href$=".pdf"]').attr('target', '_blank');
